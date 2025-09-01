01bd6b8e760bfa97e649465efdd9b66b9f728013
NicolasNun <nicolassantos9283@gmail.com>
2025-09-01 00:19:05 -0300

    tentando-integrar-login-e-cadastro-com-utils

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,14 +1,27 @@
+import { NumberUtils, PasswordUtils, StringUtils } from './Utils/index.js';
+
 const baseURL = "http://localhost:3000/usuarios";
 
-document.getElementById("btn_cadastrar").addEventListener("click", cadastrar)
+document.getElementById("btn_cadastrar").addEventListener("click", cadastrar);
+
+document.getElementById("btn_toggle_senha").addEventListener("click", () => {
+    PasswordUtils.toggleSenha("input_senha");
+});
 
 async function cadastrar() {
-    const nomeValor = document.getElementById("input_nome").value;
-    const cpfValor = document.getElementById("input_cpf").value;
-    const emailValor = document.getElementById("input_email").value;
-    const dataNascimentoValor = document.getElementById("input_data_nascimento").value;
-    const rendaMensalValor = document.getElementById("input_renda_mensal").value;
-    const senhaValor = document.getElementById("input_senha").value;
+    const nome = document.getElementById("input_nome").value;
+    const cpf = document.getElementById("input_cpf").value;
+    const email = document.getElementById("input_email").value;
+    const dataNascimento = document.getElementById("input_data_nascimento").value;
+    let rendaMensal = document.getElementById("input_renda_mensal").value;
+    const senha = document.getElementById("input_senha").value;
+
+    const { status, mensagem } = validarCampos(cpf, email, senha);
+    if (!status) return alert(mensagem);
+
+    if (rendaMensal) {
+        rendaMensal = NumberUtils.formatarMoeda(rendaMensal);
+    }
 
     try {
         await fetch(baseURL, {
@@ -17,19 +30,26 @@ async function cadastrar() {
                 "Content-type": "application/json"
             },
             body: JSON.stringify({
-                nome: nomeValor,
-                cpf: cpfValor,
-                email: emailValor,
-                dataNascimento: dataNascimentoValor,
-                rendaMensal: rendaMensalValor,
-                senha: senhaValor
+                nome,
+                cpf,
+                email,
+                dataNascimento,
+                rendaMensal,
+                senha
             })
-        })
+        });
 
-        alert("Cadastro realizado com sucesso")
-        window.location.href = "login.html"
+        alert("Cadastro realizado com sucesso!");
+        window.location.href = "login.html";
+    } catch (error) {
+        console.error(error);
+        alert("Ocorreu algum erro no processo");
     }
-    catch {
-        alert("Ocorreu algum erro no processo")
-    }
+}
+
+function validarCampos(cpf, email, senha) {
+    if (!StringUtils.validarCPF(cpf)) return { status: false, mensagem: "CPF inválido" };
+    if (!StringUtils.validarEmail(email)) return { status: false, mensagem: "Email inválido" };
+    if (senha.length < 6) return { status: false, mensagem: "A senha deve ter pelo menos 6 caracteres" };
+    return { status: true, mensagem: "" };
 }
--- js/login.js
+++ js/login.js
@@ -1,16 +1,24 @@
+import { NumberUtils, PasswordUtils, StringUtils } from "./Utils/index.js";
+
 const baseURL = "http://localhost:3000/usuarios";
 
-document.getElementById("btn_entrar").addEventListener("click", entrar)
+document.getElementById("btn_entrar").addEventListener("click", entrar);
 
 async function entrar() {
     const email = document.getElementById("input_email").value;
     const senha = document.getElementById("input_senha").value;
 
+    if (!email || !senha) {
+        alert("Preencha todos os campos!");
+        return;
+    }
+
     try {
-        const resquest = await fetch(`${baseURL}/?email=${email}&senha=${senha}`);
-        const data = await resquest.json();
+        const request = await fetch(`${baseURL}?email=${email}&senha=${senha}`);
+        const data = await request.json();
+
         if (data.length > 0) {
-            alert("Login realizado");
+            alert("Login realizado com sucesso!");
             window.location.href = "https://youtu.be/ItQoBbQGMvY?si=mHicbDn55SItJcDZ";
             return;
         }
