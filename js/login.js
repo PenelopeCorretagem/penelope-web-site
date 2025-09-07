81129eb650fd2d6503c6c6ff7e95547cbdca0ba4
RennanSMoura <rennan.moura@sptech.school>
2025-09-07 15:18:41 -0300

    Adicionando CRUD imovel e agendamento

--- js/login.js
+++ js/login.js
@@ -19,7 +19,7 @@ async function entrar() {
 
         if (data.length > 0) {
             alert("Login realizado com sucesso!");
-            window.location.href = "https://youtu.be/ItQoBbQGMvY?si=mHicbDn55SItJcDZ";
+            window.location.href = "https://www.figma.com/design/Yu6nsEwnLqOgig6hKa1ql0/Penelope-Corretora?node-id=0-1&p=f&t=37U9WZNxZxdgdyIu-0";
             return;
         }
 
