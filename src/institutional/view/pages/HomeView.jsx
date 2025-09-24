fbab21815b068817d391d2aecd137c6d46d72f65
sardinh4 <leonardo_sardinha@outlook.com>
2025-09-24 17:40:43 -0300

    feat: adiciona dependência react-router-dom e atualiza componentes ButtonView e HomeView

--- src/institutional/view/pages/HomeView.jsx
+++ src/institutional/view/pages/HomeView.jsx
@@ -3,17 +3,16 @@ import { MenuView } from '../../../shared/view/components/MenuView'
 
 export function HomeView() {
   return (
-    <>
-      <MenuView></MenuView>
+    <div className='bg-surface-primary'>
+      <MenuView isAuthenticated={true} />
+
       <CardView
         title='Título do Card'
         subtitle='Subtítulo do Card'
         description='Descrição do Card'
         category='Label do Botão'
         differences={['Diferença 1', 'Diferença 2', 'Diferença 3']}
-      >
-
-      </CardView>
-    </>
+      />
+    </div>
   )
 }
