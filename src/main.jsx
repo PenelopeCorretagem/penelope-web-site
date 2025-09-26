61fbce0855caeabe50064b75a4343ea17f0a3a11
sardinh4 <leonardo_sardinha@outlook.com>
2025-09-25 23:15:43 -0300

    feat: Remove arquivos .gitkeep não utilizados e atualiza styles.css com configuração do Tailwind CSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles.css'
+import './style.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
