74abccc044164b8e07c7d89cad21e3c464975356
kenner.lima <kenner.lima@sinqia.com.br>
2025-10-05 21:06:39 -0300

    feat: Adicionado tela Sobre e alterado componente CardImageView

--- src/institutional/view/components/CardImageView.jsx
+++ src/institutional/view/components/CardImageView.jsx
@@ -1,17 +1,54 @@
-import ContactImage from '../../assets/ContactImage.jpg'
+export function CardImageView({
+  path,
+  description,
+  position = 'bottom-right',
+  alt
+}) {
+  const getPositionClasses = () => {
+    switch (position) {
+      case 'bottom-left':
+        return '-translate-x-8 translate-y-8'
+      case 'bottom-right':
+        return 'translate-x-8 translate-y-8'
+      case 'top-left':
+        return '-translate-x-8 -translate-y-8'
+      case 'top-right':
+        return 'translate-x-8 -translate-y-8'
+      default:
+        return 'translate-x-8 translate-y-8'
+    }
+  }
 
-export function CardImageView({ descriptionImage }) {
+  const getPaddingClasses = () => {
+    switch (position) {
+      case 'bottom-left':
+        return 'pl-8 pb-8'
+      case 'bottom-right':
+        return 'pr-8 pb-8'
+      case 'top-left':
+        return 'pl-8 pt-8'
+      case 'top-right':
+        return 'pr-8 pt-8'
+      default:
+        return 'pr-8 pb-8'
+    }
+  }
+
   return (
     <div className='flex flex-col items-start gap-1.5'>
-      <div className='relative z-0 w-fit rounded-md bg-gradient-to-b from-[#B33C8E] to-[#36221D] p-4 shadow-lg'>
-        <img
-          src={ContactImage}
-          alt='Casal feliz'
-          className='border-brand-primary relative z-10 block transform-[translate(-34px,32px)] rounded-sm border-3 shadow-lg'
-        />
+      <div className={`relative w-fit ${getPaddingClasses()}`}>
+        <div className='relative z-0 w-fit rounded-md bg-brand-gradient'>
+          <div className={`relative z-10 ${getPositionClasses()} rounded-md p-0.5 bg-brand-gradient`}>
+            <img
+              src={path}
+              alt={alt}
+              className="block rounded-md shadow-lg w-full h-full object-cover"
+            />
+          </div>
+        </div>
       </div>
-      <p className='font-default-family text-text-primary mt-2 text-sm'>
-        {descriptionImage}
+      <p className='text-gray-700 mt-2 text-sm'>
+        {description}
       </p>
     </div>
   )
